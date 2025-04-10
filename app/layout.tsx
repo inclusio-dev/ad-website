51e9f170dd120e035ccdda3a1f90afd6c354c82e
Diana Bernabei <bernabei.d@gmail.com>
2025-04-10 11:11:59 +0200

    feat: updated nav link

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ interface RootLayoutProps {
 
 export default function RootLayout({ children }: RootLayoutProps) {
   return (
-    <html lang="en">
+    <html lang="it">
       <body
         className={`antialiased min-h-screen bg-white dark:bg-slate-950 text-slate-900 dark:text-slate-50 ${inter.className}`}
       >
@@ -29,7 +29,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
                 <ModeToggle />
                 <nav className="ml-auto text-sm font-medium space-x-6">
                   <Link href="/">Home</Link>
-                  <Link href="/about">About</Link>
+                  <Link href="/about">Chi siamo</Link>
                 </nav>
               </div>
             </header>
